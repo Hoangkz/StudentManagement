473b0a31fe92aae7a143ccee6b30f1a99c3a8a85
hoangkz <hoangyl245@gmail.com>
2023-04-13 06:13:50 +0700

    .env

--- StudentManagement/Program.cs
+++ StudentManagement/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using StudentManagement.Properties;
 
 namespace StudentManagement
 {
@@ -14,6 +16,18 @@ namespace StudentManagement
         [STAThread]
         static void Main()
         {
+
+            string DB_HOST = string.Format(Settings.Default.DB_HOST);
+            string connectionString = string.Format(ConfigurationManager.ConnectionStrings["StudentManagementEntities"].ConnectionString, DB_HOST);
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            // Lưu lại giá trị chuỗi kết nối mới vào file app.config
+            config.ConnectionStrings.ConnectionStrings["StudentManagementEntities"].ConnectionString = connectionString;
+            config.Save(ConfigurationSaveMode.Modified, true);
+
+            // Tải lại các giá trị cấu hình mới
+            ConfigurationManager.RefreshSection("connectionStrings");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new FormStudentManagement(2030));
